3ef9bce8bba3b9911c1e5ad6bd361630e5ccd5c2
Victor <realorth@gmail.com>
2019-09-25 10:28:17 +0200

    correoooooos

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['submit']))
+{$name = $_POST['name'];
+ 
+echo "Registo completado : <b> $name </b><br><br>";
+  $archivo="suscriptores.txt";
+  $proceso=fopen($archivo,"a") or die("basura");
+  $datos="Registro suscriptor:".$name."\r\n";
+  fwrite($proceso,$datos);
+  fclose($proceso);
+ 
+}
+?>
+
+<h1>Formulario Suscriptores alejandro</h1>
+<br>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+ <br> Email: 
+<input type="text" name="name"><br><br>
+ 
+<input type="submit" name="submit" value="Submit Form"><br>
+ 
+</form>
