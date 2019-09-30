eef43301419b696ef8e30d083472074cc503943b
Victor <realorth@gmail.com>
2019-09-30 11:08:12 +0200

    ampliaciones

--- asir1.php
+++ asir1.php
@@ -1,8 +1,14 @@
 <a>Víctor</a> 
 </br> 
-<a href="http://192.168.0.92/asir176/asir1.html"> alejandro</a>
+
+
+<p><a href="http://192.168.0.32/asir1_luis/Asir1.php"> Luis</a> <a><a>Víctor</a></a> <a href="http://192.168.0.68/asir176/Asir1.php"> alejandro</a> </p>
+
+
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,date(' y-m-d H:i'));
+fwrite($f,"\r\n");
 fclose($f)
 ?>
