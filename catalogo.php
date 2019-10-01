77473e44a4040e59e92ab7efe4b5a4b35a86ddda
Victor <realorth@gmail.com>
2019-10-01 12:29:53 +0200

    lista de perfiles

---
+++ catalogo.php
@@ -0,0 +1,27 @@
+<style>
+a{
+	display:block;
+}
+</style>
+
+<?php 
+echo "<a href='https://github.com/dblancoformacion' target=_blank>PROFE</a>";
+echo "<a href='https://github.com/AaronASIR/Asir1_aaron' target=_blank>Aaron</a>";
+echo "<a href='https://github.com/Real6/ASIR1_Ruben'target=_blank>Ruben</a>";
+echo "<a href='https://github.com/victor-zayas' target=_blank>Victor Z</a>";
+echo "<a href='https://github.com/deivisg98' target=_blank>David G</a>";
+echo "<a href='https://github.com/pablomoreta/asir1_pablo.git' target=_blank>Pablo</a>";
+echo "<a href='https://github.com/adrivg19/asir1_adrian' target=_blank>Adrian</a>";
+echo "<a href='https://github.com/MarioAlonso31info/asir1_mario' target=_blank>Mario</a>";
+echo "<a href='https://github.com/bruno38pla/ASIR1_BRUNO' target=_blank>Bruno</a>";
+echo "<a href='https://github.com/AlejandroGoVi/asir1_alex' target=_blank>Alex</a>";
+echo "<a href='https://github.com/carlosccsan/asir1-cc' target=_blank>Carlos</a>";
+echo "<a href='https://github.com/gargiljavi' target=_blank>Javi</a>";
+echo "<a href='https://github.com/sergiosamperio/asir1_sergio' target=_blank>Sergio</a>";
+echo "<a href='https://github.com/draseld/asir1_luis' target=_blank>Luis</a>";
+echo "<a href='https://github.com/alejandro-vgr/asir176' target=_blank>Alejandro</a>";
+echo "<a href='https://github.com/serran24/asir1_victor' target=_blank>Victor (YO)</a>";
+echo "<a href='https://github.com/kevin-raico-97' target=_blank>Kevin</a>";
+echo "<a href='https://github.com/rolandoMH96/Asir1-RMH' target=_blank>Rolando</a>";
+echo "<a href='https://github.com/Saioagil/asir1_saioa' target=_blank>Saioa</a>";
+
