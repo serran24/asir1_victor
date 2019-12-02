e05356b072ee84cb9fbb9b1e1149aa1bbfc2cfb1
Victor <realorth@gmail.com>
2019-12-02 11:29:22 +0100

    LAS PAJAS ESO ESgit add .!

---
+++ HTML.php
@@ -0,0 +1,92 @@
+<html lang="es">
+
+<head>
+	<title>Página Web Aleatoria Para Hablar de To</title>
+</head>
+	
+<body bgcolor="#191970" >
+
+	<center>
+	<table width="80%" height="100%" bgcolor="#0000FF"><tr><td>
+		<center>
+		<font color="white">
+		<h1>JUEGOS GUACHIS:</h1>
+		<h2>Bloodborne</h2>
+		<h3>Resumen muy resumido:</h3>
+		<h4>Noche de caza. Bloodborne cuenta la historia de una noche en la ciudad de Yharnam: una noche de cacería. Todo apunta a que algo va muy mal en esta ciudad, una misteriosa enfermedad que se apodera de sus gentes y que las convierte en bestias.Y tu tienes que matar mucho"(pero mucho)"
+		<h2>Dark Souls</h2>
+		<h3>Resumen muy resumido:</h3>
+		<h4>Los jugadores toman el papel de un personaje humano no-muerto que ha sido elegido para realizar un peregrinaje para descubrir el destino de los no-muertos. El argumento de Dark Souls se va contando fundamentalmente a través de descripciones de objetos del juego, y diálogos con personajes no jugables (PNJs).Y tu tienes que matar mucho"(pero mucho)"</h4>
+		<h2>Sekiro</h2>
+		<h3>Resumen muy resumido:</h3>
+		<h4>Está ambientado en un Japón del periodo Sengoku a finales del siglo XVI, un período de conflicto constante entre la vida y la muerte. Su protagonista es un shinobi que fue dado por muerto después de que su señor fuera secuestrado y su brazo cercenado por un líder samurái del clan Ashina.Y tu tienes que matar mucho"(pero mucho)"</h4>
+		<h2>Demons Souls</h2>
+		<h3>Resumen muy resumido:</h3>
+		<h4>La historia inicial te cuenta como un hombre conocido como “El Viejo Rey Allant”, rey de Boletaria, despertó al “Anciano”, y este trajo consigo ejércitos de demonios con el propósito de poner fin a toda la humanidad.Y tu tienes que matar mucho"(pero mucho)"</h4>
+		</font>
+		<img src="Hidetaka-Miyazaki.jpg" width="400"  height="300"/>
+		<br/>
+		<br/>
+		<p><font size=4 color="White" face="Comic Sans MS,arial,verdana">Este señor de aqui arriba es un dios,<u><b> y si lo niegas y o dudas es que o bien eres gilipollas o no has jugado a sus juegos, lo cual es peor.</b></u>Hidetaka Miyazaki (Este dios se llama Miyazaki Hidetaka, nacido en Shizuoka, Prefectura de Shizuoka, Japón). Es un director de videojuegos Japonés y actual presidente de la compañía de videojuegos From Software.<br/><u><b>La secta de este hombre mueve mas dinero que algunos paises tercermundistas.</b></u></font></p>
+		<table tr td border 2 cols></table>
+		<br/>
+		<br/>
+		<h1> <font color="white">MOVIDAS QUE HAY QUE HACER PARA ACABAR EL TRABAJO:</font> </h1>
+		<form>
+		<p> <font color="white">Dato importante 1: </font><input type="text" name="nombre" size="40"/></p>
+		<p> <font color="white">Dato importante 2: </font><input type="text" name="nacido" min="1900"/></p>
+		<p> <font color="white">Variable vital:</font>
+			<br/>
+			<input type="radio" name="hm" value="h"/><font color="white"> Dato no tan importante, pero casi 1</font>
+			<input type="radio" name="hm" value="m"/><font color="white"> Dato no tan importante, pero casi 2</font>
+		 </p>
+		 <p>
+			<input type="submit" value="Enviar"/>
+			<input type="reset" value="Borrar"/>
+		  </p>
+		</form>
+		
+		<font color="white"><table class="egt" border=1></font>
+		  <tr>
+			<th><font color="white">Provincia</font></th>
+			<th><font color="white">Prov. de...</font></th>
+		  </tr>
+		  <tr>
+			<td><font color="white">La Coruña</font></td>
+			<td><font color="white">Galicia</font></td>
+		  </tr>
+		  <tr>
+			<td><font color="white">Lugo</font></td>
+			<td><font color="white">Galicia</font></td>
+		  </tr>
+		  <tr>
+			<td><font color="white">Asturias</font></td>
+			<td><font color="white">Asturias</font></td>
+		  </tr>
+		  
+		  <tr>
+			<td><font color="white">Cantabria</font></td>
+			<td><font color="white">Cantabria</font></td>
+		  </tr>		  
+		  
+		  <tr>
+			<td><font color="white">Vizcaya</font></td>
+			<td><font color="white">País Vasco</font></td>
+		  </tr>
+		  
+		  <tr>
+			<td><font color="white">Gipuzcua</font></td>
+			<td><font color="white">País Vasco</font></td>
+		  </tr>
+		</table>
+		</center>
+	</td></tr>
+	</table>
+</center>
+</body>
+
+
+
+
+
+</html>
