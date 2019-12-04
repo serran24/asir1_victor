5e60777c4b9d49d0a4a0799a20f7b68d06077ede
Victor <realorth@gmail.com>
2019-12-04 10:46:28 +0100

    revision

--- triangulos_c.php
+++ triangulos_c.php
@@ -1,44 +1,48 @@
 <?php
 
-function longitud($a,$b,$c){
-	
-     $n="escaleno";
-	 
-	 if ($a==$b or $b==$c)
-     $n="isosceles";
- 
-	 if ($b==$c or $a==$c)
-	 $n="equilatero";
-
-	
-return $n;
-
-}
-
 function angulo($a,$b,$c){
 	
-	 $n="Acutangulo";
-     if ($a==90 or $b==90 or $c==90)
-		 
-	 $n="Rectangulo";
- 	 if ($a>90 or $b>90 or $c>90)
-		 
-	 $n="Obtusangulo";
+	$r="Triangulo Acutangulo";
 	
-return $n;
+	if ($a==90 or $b==90 or $c==90)
+		
+		$r="Triangulo Rectangulo";
+		
+		if ($a>90 or $b>90 or $c>90)
+				$r="Triangulo Obtusangulo";
+			
+			return $r;
+			
+}
+
+function longitud($d,$e,$f){
+		
+    $r="triangulo Escaleno";
+	
+	if ($d==$e || $e==$f)
+		$r="triangulo Isosceles";
+	
+	if ($e==$f and $d==$f)
+		$r="Triangulo Equilatero";
+	
+	return $r;
+	
+	
 	
 }
 
 function triangulo($a,$b,$c,$d,$e,$f){
 	
-return Angulo($a,$b,$c);
-
-return longitud($d,$e,$f);
+	return Angulo($a,$b,$c).' y '.longitud($d,$e,$f);
 	
 }
 
-echo triangulo(110,40,30,6,2,3)."<br>";
-echo triangulo(60,90,30,4,4,8)."<br>";
-echo triangulo(60,60,60,2,2,2)."<br>";
+echo triangulo(110,40,30,6,2,3).'    - El triangulo va a ser Triangulo Obtusangulo y Escaleno'."<br>"."<br>";
+echo triangulo(95,40,45,6,3,3).'    - El triangulo va a ser Triangulo Obtusangulo y Isosceles'."<br>"."<br>";
+echo triangulo(60,90,30,4,4,8).'    - El triangulo va a ser Triangulo Rectangulo y Isosceles '."<br>"."<br>";
+echo triangulo(60,90,30,4,2,8).'    - El triangulo va a ser Triangulo Rectangulo y Escaleno '."<br>"."<br>";
+echo triangulo(60,60,60,2,2,2).'    - El triangulo va a ser Triangulo  Acutangulo y Equilatero'."<br>"."<br>";
+echo triangulo(70,60,50,2,5,2).'    - El triangulo va a ser Triangulo  Acutangulo y Escaleno'."<br>"."<br>";
+echo triangulo(80,70,60,2,1,1).'    - El triangulo va a ser Triangulo  Acutangulo y Isosceles'."<br>"."<br>";
 
 ?>
